302b790d727a4d61014ab863f0b0f8cb85e47887
Samuel Anozie <samuelanozie10@yahoo.com>
2021-05-07 23:39:56 +0100

    initial commit

---
+++ dbController.js
@@ -0,0 +1,92 @@
+const UserSchema = require("./dbModel");
+
+const getAll = async (req, res) => {
+  try {
+    const users = await UserSchema.find();
+    res.status(200).json({
+      message: "Users retrieved",
+      data: { users },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      data: {},
+    });
+  }
+};
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const users = await UserSchema.findById(id);
+    res.status(200).json({
+      message: "User retrieved",
+      data: { users },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      data: {},
+    });
+  }
+};
+
+const createUser = async (req, res) => {
+  const { name, email, country } = req.body;
+
+  oldUser = await UserSchema.findOne({ email });
+  if (oldUser) {
+    res.status(400).json({
+      message: "Email already exist",
+      data: {},
+    });
+  }
+  try {
+    const user = { name, email, country };
+    await UserSchema.create(user);
+    res.status(200).json({
+      message: "Successfully created user",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      data: {},
+    });
+  }
+};
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, country } = req.body;
+  try {
+    const user = { name, email, country, _id: id };
+    await UserSchema.findByIdAndUpdate(id, user, { new: true });
+    res.status(200).json({
+      message: "Succcessfully updated User",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      data: {},
+    });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await UserSchema.findByIdAndRemove(id);
+    res.status(200).json({
+      message: "User deleted successfully.",
+      data: {},
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      data: {},
+    });
+  }
+};
+
+module.exports = { getAll, getUser, createUser, updateUser, deleteUser };
+// module.exports = { getAll, createUser, deleteUser };
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+//LOAD CONTROLLERS
+const {
+  getAll,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("./dbController");
+
+//API ROUTES
+app.post("/", createUser);
+app.get("/:id", getUser);
+app.get("/", getAll);
+app.put("/:id", updateUser);
+app.delete("/:id", deleteUser);
+
+//CONNECT DATABASE
+const CONNECTION_URL = process.env.MONGOURL;
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(console.log("Database connected"))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
+
+//START SERVER ON PORT
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on Port: http://localhost:${PORT}`);
+});
